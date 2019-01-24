b3570a707b64fd33113224c0bbac906595fea7b1
Derek Ayres <wunderlad@Hotmail.com>
2019-01-24 16:57:38 -0500

    Restructure components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Book from './components/Book'
+import HomePage from'./components/HomePage'
 import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
-import SearchBooks from './components/searchBooks'
+import SearchBooks from './components/SearchBooks'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -18,28 +19,7 @@ class BooksApp extends React.Component {
       });
     })
   }
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    //showSearchPage: false
 
-
-
-    /*onShelfChange(book, newShelf) {
-      const newBooks = this.state.books.map(currBook => {
-        if (currBook === book) {
-          currBook.shelf = newShelf;
-        }
-        return currBook;
-      });
-      this.setState({
-        books: newBooks
-      });
-    }
-    */
 
     onShelfChange = (book, newShelf) => {
       BooksAPI.update(book, newShelf);
@@ -61,81 +41,15 @@ class BooksApp extends React.Component {
     const currentlyReading =books.filter( book => book.shelf  === 'currentlyReading')
     const wantToRead =books.filter( book => book.shelf  === 'wantToRead')
     const read =books.filter( book => book.shelf  === 'read')
-    console.log(currentlyReading, wantToRead, read)
+    //console.log(currentlyReading, wantToRead, read)
     if (this.state.books.length === 0) return (  <div className="app">No Books</div> )
     else return (
       <div className="app">
         <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooks />
         )}/>
         <Route exact path="/" render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {currentlyReading.map(book => {
-                      return (
-                        <Book book={book} key={book.id} shelfChange={this.onShelfChange} />
-                      );
-                    })}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {wantToRead.map(book => {
-                      return (
-                        <Book book={book} key={book.id} shelfChange={this.onShelfChange} />
-                      );
-                    })}
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {read.map(book => {
-                      return (
-                        <Book book={book} key={book.id} shelfChange={this.onShelfChange} />
-                      );
-                    })}
-                    </ol>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
+          <HomePage />
         )}/>
         <SearchBooks/>
       </div>
---
+++ src/components/HomePage.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Book from './components/Book'
+import { Link } from 'react-router-dom'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                {currentlyReading.map(book => {
+                  return (
+                    <Book book={book} key={book.id} shelfChange={this.onShelfChange} />
+                  );
+                })}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                {wantToRead.map(book => {
+                  return (
+                    <Book book={book} key={book.id} shelfChange={this.onShelfChange} />
+                  );
+                })}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                {read.map(book => {
+                  return (
+                    <Book book={book} key={book.id} shelfChange={this.onShelfChange} />
+                  );
+                })}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HomePage
